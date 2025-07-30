14819a3c8f6ee3b99d5a20659b307845f3e212fb
TahreemAzeem <tahreemazeem8@gmail.com>
2025-07-30 19:20:41 +0500

    data

--- app/Http/Controllers/JoinController.php
+++ app/Http/Controllers/JoinController.php
@@ -17,6 +17,7 @@ class JoinController extends Controller
     // dd($request->all());
     
          $data = new JoinWeb();
+         $data->name = $request->input('name');
          $data->lastname = $request->input('lastname');
          $data->email = $request->input('email');
          $data->phone = $request->input('phone');
